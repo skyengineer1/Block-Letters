eb91d9bc4b0787c5418aeb7a8dc26dbe4bec6dea
Sky Engineer <skyengineer001@gmail.com>
2024-12-22 15:06:50 +0400

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,18 @@
+# Goga Gabelia
+# I love working with the command line. I feel like a hacker every time I use it!
+
+print(" GGG ")
+print("G   G")
+print("G    ")
+print("GGGGG")
+print("G   G")
+print("G   G")
+print(" GGG ")
+
+print(" GGG ")
+print("G   G")
+print("G    ")
+print("GGGGG")
+print("G   G")
+print("G   G")
+print(" GGG ")
